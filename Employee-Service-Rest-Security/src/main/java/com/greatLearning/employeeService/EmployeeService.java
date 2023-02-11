2b1efd9c2819e3286e7b647af13f6d98fce2e0f3
Arun Nautiyal <nautiyalar@gmail.com>
2023-02-11 10:04:08 +0530

    Add files via upload

---
+++ Employee-Service-Rest-Security/src/main/java/com/greatLearning/employeeService/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.greatLearning.employeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeService.class, args);
+		System.out.println("Hi employee security");
+	}
+}
